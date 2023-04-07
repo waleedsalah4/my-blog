0b43252d2af3341370094305072dc717cee74162
waleed salah <walidsalah652@gmail.com>
2023-04-07 19:16:19 +0200

    start implementing slug page, add functions to reat & fetch data from markdown files

---
+++ pages/posts/[...slug].js
@@ -0,0 +1,10 @@
+import PostContent from '../../components/posts/post-detail/post-content'
+
+
+function PostDetailsPage() {
+    return (
+        <PostContent />
+    )
+}
+
+export default PostDetailsPage
